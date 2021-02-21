28118d7fd51274c44155090d79b21a59b05aac81
chaneychanti <chaney.chanti@gmail.com>
2021-02-20 19:05:37 -0800

    Added sorting routes to grades and topics

--- routes/student.js
+++ routes/student.js
@@ -14,7 +14,7 @@ module.exports = function(client){
         return result;
     }
 
-    async function findOneGradeByStudent(client, studentName){
+    async function findOneGradeByName(client, studentName){
         const result = await client.db("Student_Stats").collection("Students").findOne({name:studentName});
         var grade = 0;
         if(result){
@@ -31,20 +31,46 @@ module.exports = function(client){
         }
         return result;
     }
+    async function findOneStatsByName(client, studentName){
+        var list = [];
+        const result = await client.db("Student_Stats").collection("Students").findOne({name:studentName});
+        if(result){
+            console.log(result.assignments);
+            for (const assignmentId of result.assignments) {
+                const assignment = await client.db("Student_Stats").collection("Assignments").findOne({_id: assignmentId});
+                if(assignment.is_test === false){
+                    list.push(assignment);
+                }
+            }
+            list.sort((a1, a2) => {
+                return a1.grade - a2.grade;
+            });
+            list = list.slice(0, 3);
+            console.log(list);
+                // result.assignment.sort();
+            // console.log(`${studentName}' lowest grade is assignment: `);
+            // console.log(result.assignment[0]);
+        } else {
+            console.log(`No students found with the name '${studentName}'`);
+        }
+        return list;
+    }
+
+    //------------------------------------------------------------------------------------
+
     //Get information about one student
     router.get('/:name', async (req,res) => {
         const result = await findOneStudentByName(client, req.params.name);
         res.send(result);
     });
     //Get the overall grade for one student 
-    router.get('/:name/grades', async (req,res) => {
-        const result = await findOneGradeByStudent(client, req.params.name);
-
+    router.get('/:name/grade', async (req,res) => {
+        const result = await findOneGradeByName(client, req.params.name);
         res.send(result);
     });
     //display the graphs or stats of a student
     router.get('/:name/stats', async (req,res) => {
-        const result = await findOneStatByStudent(client, req.params.name);
+        const result = await findOneStatsByName(client, req.params.name);
         res.send(result);
     });
     return router;
